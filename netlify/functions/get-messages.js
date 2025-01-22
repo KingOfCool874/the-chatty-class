b8be9b91c4573d6ab27b13662acbd5ee4c9959b0
KingOfCool874 <62258233+KingOfCool874@users.noreply.github.com>
2025-01-22 09:59:02 -0500

    Delete netlify/functions directory

--- netlify/functions/get-messages.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    const room = event.queryStringParameters.room;
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ messages: messages[room] || [] }),
-    };
-};
